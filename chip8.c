58ad86b1c7af5b62eea80650197c13b8b9cf5d90
kdu2030 <kdu2030@gmail.com>
2026-02-21 18:57:31 -0500

    Add Create Window

--- chip8.c
+++ chip8.c
@@ -1,17 +1,43 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include "SDL.h"
 
-bool init_sdl(void){
+typedef struct {
+    SDL_Window *window;
+} sdl_t;
+
+typedef struct {
+   int window_width;
+   int window_height;
+} config_t;
+
+bool init_sdl(sdl_t *sdl, const config_t config){
     if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) != 0){
         SDL_Log("Could not initialize SDL Subsystems! %s\n", SDL_GetError());
         return false;
     }
+
+    sdl->window = SDL_CreateWindow(
+        "Chip8 Emulator", 
+        SDL_WINDOWPOS_CENTERED, 
+        SDL_WINDOWPOS_CENTERED, 
+        config.window_width, 
+        config.window_height, 
+        0
+    );
+
+    if(!sdl->window){
+        SDL_Log("Could not create SDL Window %s", SDL_GetError());
+        return false;
+    }
+
     return true;
 }
 
 void final_cleanup(void){
+
     SDL_Quit();
 }
 
@@ -19,7 +45,10 @@ int main(int argc, char *argv[]){
     (void) argc;
     (void) argv;
 
-    if(!init_sdl()){
+    sdl_t sdl = {0};
+    config_t config = {0};
+
+    if(!init_sdl(&sdl, )){
         exit(EXIT_FAILURE);
     }
 
