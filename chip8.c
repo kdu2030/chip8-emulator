56f8974baddf05e569e5dbc0364932da33b12df4
kdu2030 <kdu2030@gmail.com>
2026-02-21 18:32:28 -0500

    Add Initialization and Final Cleanup Code

--- chip8.c
+++ chip8.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 #include "SDL.h"
+
+bool init_sdl(void){
+    if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) != 0){
+        SDL_Log("Could not initialize SDL Subsystems! %s\n", SDL_GetError());
+        return false;
+    }
+    return true;
+}
+
+void final_cleanup(void){
+    SDL_Quit();
+}
 
 int main(int argc, char *argv[]){
     (void) argc;
     (void) argv;
     puts("TESTING ON LINUX");    
+
+    if(!init_sdl()){
+        exit(EXIT_FAILURE);
+    }
+
+    final_cleanup();
+
+    exit(EXIT_SUCCESS);
 }
