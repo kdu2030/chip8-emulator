e7fc9bb1bbdc03690ccc1c2969c083ede30c3b8d
kdu2030 <kdu2030@gmail.com>
2026-02-22 10:11:30 -0500

    Add Chip8 Quit Handling

--- chip8.c
+++ chip8.c
@@ -17,6 +17,16 @@ typedef struct {
    uint32_t scale_factor; // Amount to scale a Chip8 Pixel by
 } config_t;
 
+typedef enum {
+    QUIT,
+    RUNNING,
+    PAUSED
+} emulator_state_t;
+
+typedef struct {
+    emulator_state_t state;
+} chip8_t;
+
 bool init_sdl(sdl_t *sdl, const config_t config){
     if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) != 0){
         SDL_Log("Could not initialize SDL Subsystems! %s\n", SDL_GetError());
@@ -61,6 +71,11 @@ bool set_config_from_args(config_t *config, int argc, char *argv[]){
     return true;
 }
 
+bool init_chip8(chip8_t *chip8){
+    chip8->state = RUNNING;
+    return true;
+}
+
 void final_cleanup(sdl_t sdl){
     SDL_DestroyRenderer(sdl.renderer);
     SDL_DestroyWindow(sdl.window);
@@ -81,6 +96,21 @@ void update_screen(const sdl_t sdl){
     SDL_RenderPresent(sdl.renderer);
 }
 
+void handle_input(chip8_t *chip8){
+    SDL_Event event;
+
+    while(SDL_PollEvent(&event)){
+        switch(event.type){
+            case SDL_QUIT:
+                chip8->state = QUIT;
+                return;
+            case SDL_KEYUP:
+                break;
+            case SDL_KEYDOWN:
+                break;
+        }
+    }
+}
 
 int main(int argc, char *argv[]){
     sdl_t sdl = {0};
@@ -94,9 +124,17 @@ int main(int argc, char *argv[]){
         exit(EXIT_FAILURE);
     }
 
+    chip8_t chip8 = {0};
+
+    if(!init_chip8(&chip8)){
+        exit(EXIT_FAILURE);
+    }
+
     clear_screen(sdl, config);
 
-    while(true){
+    while(chip8.state != QUIT){
+        handle_input(&chip8);
+
         // Delay for approximately 60 fps
         // Should be 16 - elapsed time to execute instructions
         SDL_Delay(16);
